798765f4ab606f36d4f31702e1aa90ccc3bd9276
Armann20 <arman_hovhannisyan@edu.aua.am>
2024-05-19 22:57:42 +0400

    !!!

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -77,23 +77,4 @@ public class GeneticAlgorithm {
         }
         return Collections.min(population, Comparator.comparingInt(this::evaluateTimetable));
     }
-
-    public static void main(String[] args) throws IOException {
-        String studentFile = "kfu-s-93.stu";
-        String courseFile = "kfu-s-93.crs";
-
-        Map<Integer, List<Integer>> studentExams = DataParser.parseStudents(studentFile);
-        Map<Integer, Integer> courses = DataParser.parseCourses(courseFile);
-
-        int numExams = courses.size();
-        int numTimeslots = 20; // Adjust this as necessary
-
-        GeneticAlgorithm ga = new GeneticAlgorithm(numExams, numTimeslots, studentExams);
-        List<int[]> population = ga.generateInitialPopulation(50);
-        int[] bestTimetable = ga.geneticAlgorithm(population, 100);
-        System.out.println("Best Timetable:");
-        for (int i : bestTimetable) {
-            System.out.print(i + " ");
-        }
-    }
 }
